b2bd070cce05eaccd9783ba9931769fbd2b663b4
Shriyans Singh <shriyans.singh@stonybrook.edu>
2026-04-03 18:03:36 +0000

    Add streamlit_app.py live dashboard with strategy comparison, recent trades, capital allocation

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -4,6 +4,7 @@ import os
 from core.logger import get_trades
 from metrics.risk import compute_all_metrics
 from dashboard.compare import get_strategy_returns
+from core.config import CAPITAL
 
 #configuring the streamlit page
 #emoji from https://emojiterra.com/trading/
@@ -45,3 +46,30 @@ st.dataframe(df,use_container_width=True)
 #Most recent trades is being put here now
 st.header("Recent Trades")
 all_trades=[]
+for strategy in strategies:
+    trades=get_trades(strategy)
+    if trades:#==True doesn't work for some reason
+        all_trades.extend(trades)
+if all_trades:
+    trades_df=pd.DataFrame(all_trades,columns=['id', 'timestamp', 'strategy', 'asset','action', 'price', 'quantity', 'pnl', 'reason'])
+    trades_df=trades_df.sort_values('timestamp',ascending=False)#descending from most recent
+    trades_df=trades_df.drop(columns=['id'])
+    st.dataframe(trades_df.head(20),use_container_width=True)
+else:
+    st.info("Bots are being warmed up so no trades have been logged yet")
+
+#Allocation of funds
+st.header("Capital Allocation")
+col1,col2,col3=st.columns(3)
+with col1:
+    st.metric(label="Stable Bot",value=f"${CAPITAL['stable']:,.2f}",delta="Grid Trading")
+with col2:
+    st.metric(label="Risky Bot 1", value=f"${CAPITAL['risky1']:,.2f}",delta="Momentum")
+with col3:
+    st.metric(label="Risky Bot 2",value=f"${CAPITAL['risky2']:,.2f}", delta="RL Agent")
+
+
+
+#foolter of page
+st.divider()
+st.caption("Built By Shriyans Singh github.com/Shri-2006 | Uses Polygon.io, Alpaca, XGBoost, Stable Baselines3 | (p.s. you should hire me)")
