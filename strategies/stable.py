494d80c8581e4be86009918cec1886405025a4f5
Shriyans Singh <shriyans.singh@stonybrook.edu>
2026-04-03 05:39:34 +0000

    initizalization of stable model should be done

--- strategies/stable.py
+++ strategies/stable.py
@@ -13,3 +13,20 @@ from models.regime_detector import get_regime_for_strategy
 from models.train import load_model
 from metrics.risk import compute_all_metrics
 import alpaca_trade_api as tradeapi
+
+#loading model only once to prevent forgetting previous trade, and keeping capital stored properly
+
+model=load_model("stable_model.pkl")
+#Alpaca connection (paper vs live)
+if PAPER_MODE["stable"]:
+    BASE_URL=ALPACA_PAPER_URL
+else:
+    BASE_URL=ALPACA_LIVE_URL
+
+def get_api():
+    """
+    will confirm and return the alpaca api conenctions.
+    """
+    from core.config import ALPACA_API_KEY, ALPACA_SECRET_KEY
+    return tradeapi.REST(ALPACA_API_KEY,ALPACA_SECRET_KEY,BASE_URL)
+
